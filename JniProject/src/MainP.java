2457fc2a10787cd5ead48b995c9da5fad12bd51c
usova <usovadasha@myttk.ru>
2020-12-05 22:43:56 +0700

    graalvm java fix1

--- JniProject/src/MainP.java
+++ JniProject/src/MainP.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class MainP {
     public static void main(String[] args) throws IOException  {
         Source s = Source.newBuilder("llvm", new File("/JniExample/JniProjectC/main.bc")).build();
-        Context c = Context.newBuilder().allowAllAccess(true).allowNativeAccess(true).build();
+        Context c = Context.newBuilder().allowAllAccess(true).build();
 
         Value e = c.eval(s);
         Value fb = e.getMember("getInfo");
