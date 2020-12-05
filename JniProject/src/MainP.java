6a3f0dafab451fc4de8144182662492f00cbb999
usova <usovadasha@myttk.ru>
2020-12-05 21:57:11 +0700

    graalvm

---
+++ JniProject/src/MainP.java
@@ -0,0 +1,17 @@
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+import java.io.File;
+
+public class MainP {
+    public static void main(String[] args) {
+        Source s = Source.newBuilder("llvm", new File("/JniExample/JniProject/main.bc"));
+        Context c = Context.newBuilder().allowNativeAccess(true).build();
+
+        Value fn = c.eval(s).getMember("getInfo");
+        String res = fn.execute().asString();
+
+        System.out.println(res);
+    }
+}
---
+++ JniProjectC/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int getNumbersCount(int i) {
+    int cnt = 0;
+    while(i > 0) {
+        i = i / 10;
+        cnt++;
+    }
+    return cnt;
+}
+
+char* getInfo() {
+    int size = 2;
+    char res* = (char*)malloc(sizeof(char)*size);
+    sprintf(res, "%d", 2);
+
+    return res;
+}
